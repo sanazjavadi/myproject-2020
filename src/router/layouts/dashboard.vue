824f9a376eba63e67c3daee3798c8c31a8d18d7e
sanazjavadi <sanazjavadi72@yahoo.com>
2020-01-26 11:38:55 -0500

    dashboard

---
+++ src/router/layouts/dashboard.vue
@@ -0,0 +1,15 @@
+<script></script>
+
+<template>
+  <div :class="$style.container"> </div>
+</template>
+
+<style lang="scss" module>
+@import '@design';
+
+.container {
+  min-width: $size-content-width-min;
+  max-width: $size-content-width-max;
+  margin: 0 auto;
+}
+</style>
